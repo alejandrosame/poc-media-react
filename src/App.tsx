c0808eacc0fc8c0fc35f936271e8bbdae468e13e
Alejandro Sanchez Medina <alejandrosanchzmedina@gmail.com>
2022-11-03 16:44:53 +0100

    Make camera responsive

--- src/App.tsx
+++ src/App.tsx
@@ -1,68 +1,8 @@
 import React, { useRef, useState } from "react";
-import { styled } from "@mui/material/styles";
-import Box from "@mui/material/Box";
-import Grid from "@mui/material/Grid";
-import Webcam from "react-webcam";
 
 import { Camera } from "./Camera";
 
 import "./App.css";
-
-const Item = styled(Box)(({ theme }) => ({
-  backgroundColor: theme.palette.mode === "dark" ? "#1A2027" : "#fff",
-  ...theme.typography.body2,
-  padding: theme.spacing(1),
-  color: theme.palette.text.secondary,
-  height: "100%",
-}));
-
-function FullWidthGrid() {
-  const [isShowVideo, setIsShowVideo] = useState(false);
-  const videoElement: any = useRef(null);
-
-  const videoConstraints = {
-    width: 640,
-    height: 480,
-    facingMode: "user",
-  };
-
-  const startCam = () => {
-    setIsShowVideo(true);
-  };
-
-  const stopCam = () => {
-    if (videoElement.current == null) return;
-
-    let stream = videoElement.current.stream;
-    const tracks = stream.getTracks();
-    tracks.forEach((track: any) => track.stop());
-    setIsShowVideo(false);
-  };
-
-  return (
-    <Grid className="Grid" container spacing={2} sx={{ height: "100%" }}>
-      <Grid item xs={6}>
-        <Item sx={{ backgroundColor: "#DDD8D4" }}>
-          <div className="camView">
-            {isShowVideo && (
-              <Webcam
-                audio={false}
-                ref={videoElement}
-                videoConstraints={videoConstraints}
-              />
-            )}
-          </div>
-          <p>Commands</p>
-          <button onClick={startCam}>Start Video</button>
-          <button onClick={stopCam}>Stop Video</button>
-        </Item>
-      </Grid>
-      <Grid item xs={6}>
-        <Item sx={{ backgroundColor: "#DDD8D4" }}>Right</Item>
-      </Grid>
-    </Grid>
-  );
-}
 
 function App() {
   const [isCameraOpen, setIsCameraOpen] = useState(false);
@@ -96,7 +36,6 @@ function App() {
           Close Camera
         </button>
       </footer>
-      {/*<FullWidthGrid />*/}
     </main>
   );
 }
--- src/Camera/index.js
+++ src/Camera/index.js
@@ -1,59 +1,80 @@
-import React, { useState, useRef } from "react";
-import Measure from "react-measure";
+import React, { useEffect, useRef, useState } from "react";
+import { useResizeDetector } from "react-resize-detector";
+
 import { useUserMedia } from "../hooks/use-user-media";
 import { useCardRatio } from "../hooks/use-card-ratio";
 import { useOffsets } from "../hooks/use-offsets";
+
+import "./index.css";
 
 const CAPTURE_OPTIONS = {
   audio: false,
   video: { facingMode: "environment" },
 };
 
-const Wrapper = (props) => (
-  <div className="Wrapper" {...props}>
-    {props.children}
-  </div>
+/*
+ *`returnComponent` takes care of  wrapping the component on a React.forwardRef component if it contains a ref property.
+ *
+ *For more info on ref forwarding, read: https://reactjs.org/docs/forwarding-refs.html .
+ */
+const returnComponent = ({ component, className, ...rest }) => (
+  <component className={className} {...rest} />
 );
-const Container = (props) => (
-  <div className="Container" {...props}>
-    {props.children}
-  </div>
-);
-const Canvas = (props) => (
-  <canvas className="Canvas" {...props}>
-    {props.children}
-  </canvas>
-);
-const Video = (props) => (
-  <video className="Video" {...props}>
-    {props.children}
-  </video>
-);
-const Overlay = (props) => (
-  <div className="Overlay" {...props}>
-    {props.children}
-  </div>
-);
-const Flash = (props) => (
-  <div className="Flash" {...props}>
-    {props.children}
-  </div>
-);
-const Button = (props) => (
-  <button className="Container" {...props}>
-    {props.children}
-  </button>
-);
+
+const Wrapper2 = (props) =>
+  returnComponent({ component: <div />, className: "Wrapper", ...props });
+
+const Wrapper = (props) => <div className="Wrapper" {...props} />;
+//const Container = (props) => React.forwardRef((props, ref) => <div className="Container" ref={ref} {...props} />);
+
+const Container = React.forwardRef((props, ref) => (
+  <div ref={ref} className="Container" {...props} />
+));
+
+//const Canvas = (props) => <canvas className="Canvas" {...props} />;
+const Canvas = React.forwardRef((props, ref) => (
+  <canvas ref={ref} className="Canvas" {...props} />
+));
+//const Video = (props) => <video className="Video" {...props} />;
+const Video = React.forwardRef((props, ref) => (
+  <video ref={ref} className="Video" {...props} />
+));
+const Overlay = (props) => <div className="Overlay" {...props} />;
+const Flash = (props) => <div className="Flash" {...props} />;
+const Button = (props) => <button className="Button" {...props} />;
+
+// Utilities
+/* setIfExists allows receiving a transformed value if a property exists in an object.
+ *
+ * With this function, now we can type
+ *     setIfExists(videoRef.current, "videoWidth", (value)=>`${value}px`)
+ * instead of
+ *     videoRef.current && videoRef.current.videoWidth && `${videoRef.current.videoWidth}px`
+ */
+const setIfExists = (object, property, transform) => {
+  if (object == undefined) return undefined;
+  else {
+    const child = object[property];
+    if (child != undefined) {
+      return transform(child);
+    }
+  }
+  return undefined;
+};
 
 export const Camera = ({ onCapture, onClear }) => {
+  // Set references
   const canvasRef = useRef();
   const videoRef = useRef();
+  const { width, height, ref: resizeRef } = useResizeDetector();
 
-  const [container, setContainer] = useState({ width: 0, height: 0 });
+  // Set states
+  const [container, setContainer] = useState({ width: 100, height: 300 });
   const [isVideoPlaying, setIsVideoPlaying] = useState(false);
   const [isCanvasEmpty, setIsCanvasEmpty] = useState(true);
   const [isFlashing, setIsFlashing] = useState(false);
 
+  // Set extra utility variables
   const mediaStream = useUserMedia(CAPTURE_OPTIONS);
   const [aspectRatio, calculateRatio] = useCardRatio(1.586);
   const offsets = useOffsets(
@@ -63,17 +84,22 @@ export const Camera = ({ onCapture, onClear }) => {
     container.height
   );
 
+  // Handle container resizes
+  useEffect(() => {
+    if (width) {
+      setContainer({
+        width: width,
+        height: Math.round(width / aspectRatio),
+      });
+    }
+  }, [width]);
+
+  // Update video reference
   if (mediaStream && videoRef.current && !videoRef.current.srcObject) {
     videoRef.current.srcObject = mediaStream;
   }
 
-  const handleResize = (contentRect) => {
-    setContainer({
-      width: contentRect.bounds.width,
-      height: Math.round(contentRect.bounds.width / aspectRatio),
-    });
-  };
-
+  // Callbacks
   const handleCanPlay = () => {
     calculateRatio(videoRef.current.videoHeight, videoRef.current.videoWidth);
     setIsVideoPlaying(true);
@@ -108,75 +134,71 @@ export const Camera = ({ onCapture, onClear }) => {
   };
 
   if (!mediaStream) {
+    //alert("No media stream found. Check if your camera is active");
     return null;
   }
 
-  return (
-    <Measure bounds onResize={handleResize}>
-      {({ measureRef }) => (
-        <div ref={measureRef} className="container">
-          {JSON.stringify(container)}
-        </div>
-      )}
-    </Measure>
-  );
   /*
-  containerMaxHeight={videoRef.current && videoRef.current.videoHeight}
-  containetMaxWidth={videoRef.current && videoRef.current.videoWidth}
-  return (
-    <Measure bounds onResize={handleResize}>
-      {({ measureRef }) => (
-        <Wrapper>
-          <Container
-            ref={measureRef}
-            style={{
-              height: `${container.height}px`,
-              max-width: ${() => containerMaxWidth && `${containerMaxWidth}px`};
-              max-height: ${({ maxHeight }) => containerMaxHeight && `${containerMaxHeight}px`};  
-            }}
-          >
-            <Video
-              ref={videoRef}
-              hidden={!isVideoPlaying}
-              onCanPlay={handleCanPlay}
-              autoPlay
-              playsInline
-              muted
-              style={{
-                top: `-${offsets.y}px`,
-                left: `-${offsets.x}px`,
-              }}
-            />
-
-            <Overlay hidden={!isVideoPlaying} />
-
-            <Canvas
-              ref={canvasRef}
-              width={container.width}
-              height={container.height}
-            />
-
-            <Flash
-              style={${({ flash }) => {
-                  if (flash) {
-                    return css`
-                      animation: ${flashAnimation} 750ms ease-out;
-                    `;
-                  }
-                }}}
-              flash={isFlashing.toString()}
-              onAnimationEnd={() => setIsFlashing(false)}
-            />
-          </Container>
-
-          {isVideoPlaying && (
-            <Button onClick={isCanvasEmpty ? handleCapture : handleClear}>
-              {isCanvasEmpty ? "Take a picture" : "Take another picture"}
-            </Button>
-          )}
-        </Wrapper>
-      )}
-    </Measure>
-  );
+  style={${({ flash }) => {
+                    if (flash) {
+                      return css`
+                        animation: ${flashAnimation} 750ms ease-out;
+                      `;
+                    }
+                  }}}
   */
+  return (
+    <Wrapper>
+      <Container
+        ref={resizeRef}
+        style={{
+          height: `${container.height}px`,
+          maxWidth: () =>
+            setIfExists(
+              videoRef.current,
+              "videoWidth",
+              (value) => `${value}px`
+            ),
+          maxHeight: () =>
+            setIfExists(
+              videoRef.current,
+              "videoHeight",
+              (value) => `${value}px`
+            ),
+        }}
+      >
+        <Video
+          ref={videoRef}
+          hidden={!isVideoPlaying}
+          onCanPlay={handleCanPlay}
+          autoPlay
+          playsInline
+          muted
+          style={{
+            top: `-${offsets.y}px`,
+            left: `-${offsets.x}px`,
+          }}
+        />
+
+        <Overlay hidden={!isVideoPlaying} />
+
+        <Canvas
+          ref={canvasRef}
+          width={container.width}
+          height={container.height}
+        />
+
+        <Flash
+          flash={isFlashing.toString()}
+          onAnimationEnd={() => setIsFlashing(false)}
+        />
+      </Container>
+
+      {isVideoPlaying && (
+        <Button onClick={isCanvasEmpty ? handleCapture : handleClear}>
+          {isCanvasEmpty ? "Take a picture" : "Take another picture"}
+        </Button>
+      )}
+    </Wrapper>
+  );
 };
